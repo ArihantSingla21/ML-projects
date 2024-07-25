60efe8678e14d287d8d442c39af32b77cddea4a5
ArihantSingla21 <arihantsingla21@gmail.com>
2024-07-25 20:06:38 +0530

    data transformation

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys 
 from src.logger import logging
 
-
+## this helps us to raise a custom message of your own
 def error_message_details(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
+    _,_,exc_tb=error_detail.exc_info() ## gives us error details 
     filename=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name[{0}] line number [{1}] error message[{2}]".format(filename,exc_tb.tb_lineno,str(error))
     
--- src/logger.py
+++ src/logger.py
@@ -3,14 +3,14 @@ import os
 from datetime import datetime 
 
 
-LOG_FILE=f"{datetime.now().strftime('%m _%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" 
+log_path=os.path.join(os.getcwd(),"logs",LOG_FILE) ## this creates the log path 
 os.makedirs(log_path,exist_ok=True)
 
-LOG_FILE_PATH= os.path.join(log_path,LOG_FILE)
+LOG_FILE_PATH= os.path.join(log_path,LOG_FILE) ## this creates the log file path
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=LOG_FILE_PATH, ## where to save the log files 
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
