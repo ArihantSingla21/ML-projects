a8229e7c8c63bb9c79ec39808df8fb669d7eed35
ArihantSingla21 <arihantsingla21@gmail.com>
2024-07-27 02:53:51 +0530

    flask application

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 import dill
 import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         ## this is how we get the directory name
@@ -20,11 +21,17 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
         
         for i in range(len(models)):
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model= list(models.values())[i]
             
             model.fit(x_train,y_train)
@@ -40,3 +47,10 @@ def evaluate_model(x_train,y_train,x_test,y_test,models):
             
     except Exception as e:
         raise CustomException(e,sys)        
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as obj:
+            return dill.load(obj)       
+    except Exception as e:
+        raise CustomException(e,sys)    
